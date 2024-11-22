9e0eb5621971e81cd37fa22546e1128ecfc3e590
Micah Alpern <malpern@alpern.org>
2024-11-21 19:53:34 -0800

    Initial Commit

---
+++ LightsOut/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LightsOut
+//
+//  Created by Micah Alpern on 11/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LightsOut/LightsOutApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightsOutApp.swift
+//  LightsOut
+//
+//  Created by Micah Alpern on 11/21/24.
+//
+
+import SwiftUI
+
+@main
+struct LightsOutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
