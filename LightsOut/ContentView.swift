b82265a14eb1c572664d15788ec7bf8474864157
Micah Alpern <malpern@alpern.org>
2024-11-22 19:57:53 -0800

    working, sent to scott

--- LightsOut/ContentView.swift
+++ LightsOut/ContentView.swift
@@ -8,14 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isOn: Bool = true
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(gradient: Gradient(
+                colors: isOn ? [.red, .yellow] : [.black]),
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            .ignoresSafeArea()
+            .animation(.easeInOut, value: isOn)
+
+            VStack {
+                Image(systemName:  isOn ? "lightbulb" : "lightbulb.fill")
+                    .renderingMode(.template)
+                    .foregroundColor(isOn ? .black : .white)
+                    .font(.system(size: 40))
+                    .shadow(color: isOn ? .clear : .white, radius: isOn ? 0 : 10)
+                    .shadow(color: isOn ? .clear : .white, radius: isOn ? 0 : 10)
+
+                
+                Toggle("Lights out, Micah!", isOn: $isOn)
+                    .font(.system(size: 35))
+                    .foregroundColor(isOn ? .black : .white)
+                    .tint(.green) // Optional: Change toggle color
+                    .padding() // Add some padding to the toggle
+                    .shadow(color: isOn ? .clear : .white, radius: isOn ? 0 : 10)
+                    .shadow(color: isOn ? .clear : .white, radius: isOn ? 0 : 10)
+                    .sensoryFeedback(.selection, trigger: isOn)
+                    .animation(.easeInOut, value: isOn)
+
+                }
+            .padding()
         }
-        .padding()
     }
 }
 
